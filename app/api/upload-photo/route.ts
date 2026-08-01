0e7ad9f3f771ce8cf47bdd885f0d68922826219d
Arunan-Kavirajan <arunan.kavirajan@gmail.com>
2026-08-01 19:33:54 +0530

    Upload Bug fixes

--- app/api/upload-photo/route.ts
+++ app/api/upload-photo/route.ts
@@ -41,6 +41,7 @@ export async function POST(request: Request) {
       headers: {
         Authorization: `Bearer ${GITHUB_TOKEN}`,
         Accept: "application/vnd.github+json",
+        "Content-Type": "application/json",
         "X-GitHub-Api-Version": "2022-11-28",
       },
       body: JSON.stringify({
